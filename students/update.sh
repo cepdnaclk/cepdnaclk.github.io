417de2041bf6bb8961c9b74611cd39e61c8a0487
Gihan Chanaka Jayatilaka <gihanchanaka@gmail.com>
2020-03-19 19:33:41 +0530

    Update update.sh

--- students/update.sh
+++ students/update.sh
@@ -8,40 +8,89 @@ echo "<html>
 <ul>" > index.html
 
 
+GITHUBNAME="chandimab"
+NAME="chandimab"
+rm -rf "./$NAME/"
+git clone "https://github.com/$GITHUBNAME/$GITHUBNAME.github.io.git"
+mv "$GITHUBNAME.github.io" "$NAME"
+echo "<li><a href=\"https://tesla.ce.pdn.ac.lk/students/$NAME/\">$NAME</a></li>" >> index.html
+echo $NAME "completed"
+
+
+
+GITHUBNAME="hiruna72"
+NAME="hirunas"
+rm -rf "./$NAME/"
+git clone "https://github.com/$GITHUBNAME/$GITHUBNAME.github.io.git"
+mv "$GITHUBNAME.github.io" "$NAME"
+echo "<li><a href=\"https://tesla.ce.pdn.ac.lk/students/$NAME/\">$NAME</a></li>" >> index.html
+echo $NAME "completed"
+
+
+
+GITHUBNAME="HishanIndrajith"
+NAME="hishana"
+rm -rf "./$NAME/"
+git clone "https://github.com/$GITHUBNAME/$GITHUBNAME.github.io.git"
+mv "$GITHUBNAME.github.io" "$NAME"
+echo "<li><a href=\"https://tesla.ce.pdn.ac.lk/students/$NAME/\">$NAME</a></li>" >> index.html
+echo $NAME "completed"
+
+
+
+
 GITHUBNAME="gihanjayatilaka"
 NAME="gihanj"
 rm -rf "./$NAME/"
 git clone "https://github.com/$GITHUBNAME/$GITHUBNAME.github.io.git"
 mv "$GITHUBNAME.github.io" "$NAME"
+echo "<li><a href=\"https://tesla.ce.pdn.ac.lk/students/$NAME/\">$NAME</a></li>" >> index.html
 echo $NAME "completed"
 
-echo "<li><a href=\"https://tesla.ce.pdn.ac.lk/students/$NAME/\">$NAME</a></li>" >> index.html
 
-
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#		The entries below are not of the generic format
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 GITHUBNAME="Mabeesha"
 NAME="mabeeshaw"
 rm -rf "./$NAME/"
 git clone "https://github.com/$GITHUBNAME/$GITHUBNAME.github.io.git"
+cd "$GITHUBNAME.github.io"
+git checkout 0d97480
+cd ../
 mv "$GITHUBNAME.github.io" "$NAME"
+echo "<li><a href=\"https://tesla.ce.pdn.ac.lk/students/$NAME/\">$NAME</a></li>" >> index.html
 echo $NAME "completed"
 
-echo "<li><a href=\"https://tesla.ce.pdn.ac.lk/students/$NAME/\">$NAME</a></li>" >> index.html
+
+
+
 
 NAME="nuwanj"
 rm -rf "./$NAME/"
-git clone https://github.com/NuwanJ/my-site
+git clone https://github.com/NuwanJ/my-site.git
 mv my-site "$NAME"
+echo "<li><a href=\"https://tesla.ce.pdn.ac.lk/students/$NAME/\">$NAME</a></li>" >> index.html
 echo $NAME "completed"
 
-echo "<li><a href=\"https://tesla.ce.pdn.ac.lk/students/$NAME/\">$NAME</a></li>" >> index.html
 
 GITHUBNAME="SanojPunchihewa"
 NAME="sanojp"
 rm -rf "./$NAME/"
 git clone "https://github.com/$GITHUBNAME/$GITHUBNAME.github.io.git"
 mv "$GITHUBNAME.github.io" "$NAME"
+echo "<li><strike><a href=\"https://tesla.ce.pdn.ac.lk/students/$NAME/\">$NAME</a><strike></strike></li>" >> index.html
 echo $NAME "completed"
 
-echo "<li><a href=\"https://tesla.ce.pdn.ac.lk/students/$NAME/\">$NAME</a></li>" >> index.html
+
+
+
+
+
+
+
+
+
+
 
 echo "	</ul></body></html>" >> index.html
