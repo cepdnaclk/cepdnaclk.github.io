c9729627e3bd23b14752590637da2a84b6f7d825
gihanjayatilaka <gihanchanaka@gmail.com>
2020-04-21 00:46:42 +0530

    co222/2020

--- sites/marks/CO222/2020/js/draw.js
+++ sites/marks/CO222/2020/js/draw.js
@@ -1,21 +1,34 @@
 
 var file = "marks.csv";
+var namefile = "names.csv";
 var assignements;
 var lines;
 var allmarks=[];
+var stackedMarks = [];
+var namesArray = [];
 (function getFile(){
     $.get(file,function(txt){
         lines = txt.split("\n");
 		assignements = lines[0].split(",").slice(1); //assignements from first line
 		var marks = lines.slice(1);
+
+		var arrayTofillDropDown = [];
         for (var i = 0; i < marks.length; i++) {
 			var oneMark = marks[i].split(",");
+			
+			if(oneMark.length == assignements.length+1){
+				//console.dir(oneMark);
+				var obj = {}
+				obj.name = oneMark[0];
+				obj.data = oneMark.slice(1).map(parseFloat);
+				stackedMarks.push(obj);
+			}
 			//if not empty line
 			if(oneMark.length == assignements.length +1){
 				var obj = {};
 				obj.name = oneMark[0]; //extracting name
 				if(!(obj.name.toLowerCase()==="maximum")){
-						$("#dropdown").append( '<li><a href="#">' + obj.name + '</a></li>' );//appending names
+						arrayTofillDropDown.push(obj.name);
 				}
 				var data = oneMark.slice(1).map(parseFloat); //extracting marks
 				
@@ -31,10 +44,181 @@ var allmarks=[];
             
         }
 		
+        //sort E numbers
+        arrayTofillDropDown = arrayTofillDropDown.sort();
+        for(var i=0;i<arrayTofillDropDown.length;i++){
+        	$("#dropdown").append( '<li><a href="#">' + arrayTofillDropDown[i] + '</a></li>' );//appending names
+        }
+
 		plotChart(allmarks);
+		drawStacked(stackedMarks,false);
+		$('#sortcheck').click(function() {
+			if(this.checked){
+				drawStacked(stackedMarks,true);
+			}else{
+				drawStacked(stackedMarks,false);
+			}
+			//
+		})
 		
     }); 
 })();
+
+
+(function getFileName(){
+    $.get(namefile,function(txt){
+                        console.dir(txt);
+        var lines = txt.split("\n");
+                var _ = lines[0].split(","); //assignements from first line
+                var names = lines.slice(1);
+
+        for (var i = 0; i < names.length; i++) {
+			if (names[i].length != 0){
+                        	var linex = names[i];
+                        	console.dir(linex);
+				var line = linex.split(",");
+
+                        //if not empty line
+				if(line.length == 2){
+                                	var obj = {};
+                               	 	obj.eNo = line[0]; //extracting name
+					obj.name= line[1];                              
+					namesArray.push(obj);
+                	        }
+			}
+
+        }
+});
+
+})();
+
+function getName(eno){
+	for (var i = 0; i < namesArray.length; i++){
+		if (namesArray[i].eNo == eno)
+			return namesArray[i].name;
+	}
+	return "";
+}
+
+
+
+
+function getSum(total, num) {
+    return total + num;
+}
+//numbers.reduce(getSum);
+
+
+
+
+
+function drawStacked(dataIn,sorted){
+		//console.dir(data);
+		var categorylist = [];
+		var seriesSet = [];
+		var data=JSON.parse(JSON.stringify(dataIn));
+		if(sorted){
+			console.log('going to sort array');
+			// I am going to do the insertion sort
+			data = data.sort(function(a, b) {
+			    return  b.data.reduce(getSum) - a.data.reduce(getSum);
+			});
+
+		}
+
+
+		for(var i=0;i<data.length;i++){
+			categorylist.push(data[i].name);
+		}
+		for(var i = assignements.length-1 ;i>=1;i--){
+			var obj = {};
+			obj.name = assignements[i];
+			obj.data = [];
+			for(var j=0;j<data.length;j++){
+				obj.data.push(data[j].data[i]);
+			}
+			seriesSet.push(obj);
+
+		}
+		console.dir(seriesSet);
+
+		$('#stacked').highcharts({
+	    chart: {
+	        type: 'column'
+	    },
+	    title: {
+	        text:  'Total Marks',
+	    },
+	    xAxis: {
+	        categories: categorylist
+	    },
+	    yAxis: {
+	        min: 0,
+	        title: {
+	            text: 'Total marks'
+	        }// },
+	        // stackLabels: {
+	        //     enabled: false,
+	        //     style: {
+	        //         fontWeight: 'bold',
+	        //         color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+	        //     }
+	        // }
+	    },
+	    legend: {
+	        align: 'right',
+	        x: -30,
+	        verticalAlign: 'top',
+	        y: 25,
+	        floating: true,
+	        backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+	        borderColor: '#CCC',
+	        borderWidth: 1,
+	        shadow: false
+	    },
+	    tooltip: {
+	        formatter: function() {
+	        	total = getTotal(this.x);
+	        	position = getPosition(this.x);
+			var name = getName(this.x);
+
+	        	console.dir(name);
+	        	imagePath = '"http://www.ce.pdn.ac.lk/images/people/e14/'+this.x.toLowerCase().split('/').join('')+'.jpg"';
+	        	if(this.x.toLowerCase() == 'maximum'){
+	        		imagePath = "";
+	        	}
+	        	console.log(imagePath);
+	        	
+		        return '<b>'+this.series.name +'</b> '+ this.y + '<br>Total '+total+'<br>Position '+position+'<br><img class="tooltipImage" src='+imagePath+'>'+'<br><center><b>' + this.x + '</b></center><center><b>' + name + '</b></center>';
+		    },
+		    useHTML: true
+	    },
+	    plotOptions: {
+	        column: {
+	            stacking: 'normal',
+	            dataLabels: {
+	                enabled: false,
+	                color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+	            }
+	        },
+	        series: {
+	            point: {
+	                events: {
+	                    click: function () {
+	                        console.dir(this.category);
+	                        drawLinechart(this.category)
+	                    }
+	                }
+	            }
+        	}
+	    },
+	    series: seriesSet
+	});
+}
+
+
+
+
 
 
 function plotChart(data){
@@ -71,4 +255,7 @@ function plotChart(data){
 				},
 				series: data
 			});
+
 }
+
+
